42851d4ae9b56c0dfb5002ef7019a39b47b25a3b
Glaucio Melo <glaucio.melo@gmail.com>
2017-09-17 20:00:00 +0200

    adding license information

--- gm-java/src/main/java/br/com/gm2/core/strategy/impl/BruteForceStrategy.java
+++ gm-java/src/main/java/br/com/gm2/core/strategy/impl/BruteForceStrategy.java
@@ -1,14 +1,36 @@
+/**
+ * Copyright (C) {2017}  {Glaucio Melo}
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package br.com.gm2.core.strategy.impl;
 
 import br.com.gm2.core.element.Crumb;
 import br.com.gm2.core.strategy.UnpackStrategy;
 
+/**
+ * Implementation of brute force strategy.
+ * 
+ * @author glauciom
+ *
+ */
 public class BruteForceStrategy implements UnpackStrategy {
 
 	@Override
 	public byte[] execute(Crumb crumb) {
 		// TODO Auto-generated method stub
-		return new byte[] {49};
+		return new byte[] { 49 };
 	}
 
 }
