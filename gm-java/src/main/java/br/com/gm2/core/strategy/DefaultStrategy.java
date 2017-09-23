ebf3399e64bcc03a6a2ed561c510f3db92634bc4
Glaucio Melo <glaucio.melo@gmail.com>
2017-09-23 08:41:42 +0200

    adding licensing text

--- gm-java/src/main/java/br/com/gm2/core/strategy/DefaultStrategy.java
+++ gm-java/src/main/java/br/com/gm2/core/strategy/DefaultStrategy.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) {2017}  {Glaucio Melo}
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package br.com.gm2.core.strategy;
 
 import java.io.UnsupportedEncodingException;
@@ -5,6 +21,11 @@ import java.security.NoSuchAlgorithmException;
 
 import br.com.gm2.core.element.Crumb;
 
+/**
+ * Main algorithm entry point.
+ * @author glauciom
+ *
+ */
 public interface DefaultStrategy {
 	
     public byte[] execute(Crumb crumb) throws NoSuchAlgorithmException, UnsupportedEncodingException;
--- gm-java/src/main/java/br/com/gm2/core/strategy/impl/AbstractStrategy.java
+++ gm-java/src/main/java/br/com/gm2/core/strategy/impl/AbstractStrategy.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) {2017}  {Glaucio Melo}
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package br.com.gm2.core.strategy.impl;
 
 import java.io.UnsupportedEncodingException;
@@ -7,6 +23,11 @@ import br.com.gm2.core.element.Crumb;
 import br.com.gm2.core.strategy.DefaultStrategy;
 import br.com.gm2.core.strategy.UnpackStrategy;
 
+/**
+ * Adding common capabilities to all Strategies.
+ * @author glauciom
+ *
+ */
 public abstract class AbstractStrategy implements DefaultStrategy, UnpackStrategy {
 
 	protected int[] subset;
